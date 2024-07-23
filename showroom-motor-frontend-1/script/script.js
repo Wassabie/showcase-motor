97b0dc2e0ec73669e7b751026ae04a4a41475c59
Wassabie <134603564+Wassabie@users.noreply.github.com>
2024-07-23 11:57:00 +0700

    Delete showroom-motor-frontend-1 directory

--- showroom-motor-frontend-1/script/script.js
+++
@@ -1,9 +0,0 @@
-function showForm() {
-    const elm = document.getElementById('modal');
-    elm.style.display('flex');
-}
-
-function closeForm() {
-    const elm = document.getElementById('modal');
-    elm.style.display('none');
-}
