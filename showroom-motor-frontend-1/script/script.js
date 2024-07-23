12563dfe011435c126d7de1dbb4eec7b11e892b3
Wassabie <134603564+Wassabie@users.noreply.github.com>
2024-07-23 11:56:11 +0700

    Add files via upload

---
+++ showroom-motor-frontend-1/script/script.js
@@ -0,0 +1,9 @@
+function showForm() {
+    const elm = document.getElementById('modal');
+    elm.style.display('flex');
+}
+
+function closeForm() {
+    const elm = document.getElementById('modal');
+    elm.style.display('none');
+}
